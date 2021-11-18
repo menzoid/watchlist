f43c5ffa513fabb5e5c97f18c0e650fc92079d1b
Ryan Brouwer <mennofromheavan@gmail.com>
2021-11-18 16:08:28 +0000

    finidshed all backend fuctiuonality, great day with Mario

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+50.times do
+  Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.paragraph(sentence_count: 4),
+    poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+    rating: rand(1.0..10.0)
+  )
+
+end

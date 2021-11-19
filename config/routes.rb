19b6f8d2c144960067d4bf330b5e1175f7c57685
Ryan Brouwer <mennofromheavan@gmail.com>
2021-11-19 13:55:14 +0000

    minor change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- root to: 'lists#index'
+  root to: 'lists#index'
 
-  resources :lists, only: [:show, :create, :new, :destroy] do
+  resources :lists, only: [:show, :index, :create, :new, :destroy] do
     resources :bookmarks, only: [:create, :new]
   end
   resources :bookmarks, only: [:destroy]

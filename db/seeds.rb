ea2bee4f1523c57f63389c9d9f4133b11a07e9c3
Ryan Brouwer <mennofromheavan@gmail.com>
2021-11-19 14:37:43 +0000

    adding new seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,43 @@
 require 'open-uri'
 require 'json'
 
-url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
 
-data = URI.open(url).read
-result = JSON.parse(data)
-
-results_only = result['results']
-poster_prefix = 'https://image.tmdb.org/t/p/w500'
-results_only.each do |movie|
-  poster_path = movie['poster_path']
-  Movie.create!(
-    title: movie['title'],
-    overview: movie['overview'],
-    poster_url: "#{poster_prefix}#{poster_path}",
-    rating: movie['vote_average'].to_f
-  )
+url = "http://tmdb.lewagon.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
+puts "Movies created"
+
+# require 'open-uri'
+# require 'json'
+
+# url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
+
+# data = URI.open(url).read
+# result = JSON.parse(data)
+
+# results_only = result['results']
+# poster_prefix = 'https://image.tmdb.org/t/p/w500'
+# results_only.each do |movie|
+#   poster_path = movie['poster_path']
+#   Movie.create!(
+#     title: movie['title'],
+#     overview: movie['overview'],
+#     poster_url: "#{poster_prefix}#{poster_path}",
+#     rating: movie['vote_average'].to_f
+#   )
+# end

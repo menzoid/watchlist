cc4f495d6a4f4b224634ab4ea1ace709be0040a2
Ryan Brouwer <mennofromheavan@gmail.com>
2021-11-19 23:44:58 +0000

    added photos and made db larger

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -32,6 +32,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :description)
+    params.require(:list).permit(:name, :description, :photo)
   end
 end
